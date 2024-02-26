4a024c3cb03b0c7b04aee11b9deac4ec31575e4c
Dmitrii Aleksandrov <aleksandrov.d89@gmail.com>
2024-02-26 01:36:32 -0300

    Update default.nix

--- home/programs/mimeo/default.nix
+++ home/programs/mimeo/default.nix
@@ -1,12 +1,12 @@
 { pkgs, lib, ... }:
 
 let
-#  associations = lib.secretManager {
- #   filepath = ../../secrets/mimeo-associations.txt;
-  #  fileAction = lib.readFile;
-   # encryptedSha256 = "255dcce51ca73d2370bbdf1bf7e5c3f894da2b85049a2cec91e4afecb66e2087";
-    #emptyValue = "";
-  #};
+  associations = lib.secretManager {
+    filepath = ../../secrets/mimeo-associations.txt;
+    fileAction = lib.readFile;
+    encryptedSha256 = "255dcce51ca73d2370bbdf1bf7e5c3f894da2b85049a2cec91e4afecb66e2087";
+    emptyValue = "";
+  };
 
    # xdg-utils as a runtime dependency: https://github.com/NixOS/nixpkgs/pull/181171
   xdg-mimeo = pkgs.xdg-utils;
@@ -19,5 +19,5 @@ let
 in
 {
   home.packages = [ pkgs.mimeo xdg-mimeo ];
- # xdg.configFile."mimeo/associations.txt".text = associations;
+  xdg.configFile."mimeo/associations.txt".text = associations;
 }

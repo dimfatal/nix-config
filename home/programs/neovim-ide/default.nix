cc7ad8e849429e8a68f2abb4f0f0dca95560930c
Dmitrii Aleksandrov <aleksandrov.d89@gmail.com>
2024-02-25 23:51:54 -0300

    Update default.nix

--- home/programs/neovim-ide/default.nix
+++ home/programs/neovim-ide/default.nix
@@ -3,12 +3,12 @@
 let
   metals = pkgs.callPackage ./metals.nix { };
 
-  openaiApiKey = lib.secretManager {
-    filepath = ../../secrets/openai-api-key;
-    fileAction = file: lib.removeNewline (lib.readFile file);
-    encryptedSha256 = "80c866e45d4344f12fa4d98d29cadd27d31d68fd402c97fe05eced6b470ebae8";
-    emptyValue = "SECRET";
-  };
+  # openaiApiKey = lib.secretManager {
+    # filepath = ../../secrets/openai-api-key;
+    # fileAction = file: lib.removeNewline (lib.readFile file);
+    # encryptedSha256 = "80c866e45d4344f12fa4d98d29cadd27d31d68fd402c97fe05eced6b470ebae8";
+    # emptyValue = "SECRET";
+  # };
 in
 {
   programs.neovim-ide = {
@@ -121,10 +121,10 @@ in
           enable = true;
           glow.enable = true;
         };
-        chatgpt = {
-          enable = true;
-          inherit openaiApiKey;
-        };
+        # chatgpt = {
+        #   enable = true;
+      #     inherit openaiApiKey;
+    #     };
         git = {
           enable = true;
           gitsigns.enable = true;

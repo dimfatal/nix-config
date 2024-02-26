fbd63975c5057cd1222356a2c214a55295f5fc70
Dmitrii Aleksandrov <aleksandrov.d89@gmail.com>
2024-02-25 23:19:00 -0300

    Update default.nix

--- home/programs/mimeo/default.nix
+++ home/programs/mimeo/default.nix
@@ -8,12 +8,14 @@ let
     emptyValue = "";
   };
 
-  # xdg-utils as a runtime dependency: https://github.com/NixOS/nixpkgs/pull/181171
-  xdg-mimeo = pkgs.xdg-utils.overrideAttrs (old: {
-    postInstall = ''
-      cp ${pkgs.mimeo}/bin/mimeo $out/bin/xdg-open
-    '' + old.postInstall;
-  });
+   # xdg-utils as a runtime dependency: https://github.com/NixOS/nixpkgs/pull/181171
+  xdg-mimeo = pkgs.xdg-utils;
+  # .overrideAttrs (old: {
+  #   postInstall = ''
+  #     cp ${pkgs.mimeo}/bin/mimeo $out/bin/xdg-open
+  #   '' + old.postInstall;
+  # });
+
 in
 {
   home.packages = [ pkgs.mimeo xdg-mimeo ];
